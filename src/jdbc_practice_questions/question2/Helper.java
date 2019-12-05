6a7828f4408067c14104fb8addabbb4be56ea31d
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-12-05 22:08:13 +0530

    addNewCourseInDatabase method created

--- src/jdbc_practice_questions/question2/Helper.java
+++ src/jdbc_practice_questions/question2/Helper.java
@@ -6,4 +6,21 @@ import java.sql.ResultSet;
 import java.sql.PreparedStatement;
 
 public class Helper {
+    public boolean addNewCourseInDatabase(Connection connection, String[] course){
+        boolean isAdded=false;
+        try{
+            PreparedStatement preparedStatement= connection.prepareStatement("INSERT INTO courses(courseID,courseName,courseCode,courseDuration,courseCost)" +
+                    " VALUES(?,?,?,?,?);");
+            preparedStatement.setInt(1,Integer.parseInt(course[0]));
+            preparedStatement.setString(2,course[1]);
+            preparedStatement.setString(3,course[2]);
+            preparedStatement.setInt(4,Integer.parseInt(course[3]));
+            preparedStatement.setDouble(5,Double.parseDouble(course[4]));
+            preparedStatement.executeUpdate();
+            isAdded=true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return isAdded;
+    }
 }
