fdee5377b44c7faced9b08f4f1f510d8b9362a0d
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-12-05 21:23:24 +0530

    sql classes imported

--- src/jdbc_practice_questions/question2/Helper.java
+++ src/jdbc_practice_questions/question2/Helper.java
@@ -1,4 +1,9 @@
 package jdbc_practice_questions.question2;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+
 public class Helper {
 }
