688ccf6dc7342646d2f3a395df877c68f95017dd
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-12-05 20:47:06 +0530

    getConnection and attemptConnection method created

--- src/jdbc_practice_questions/question2/JdbcConnection.java
+++ src/jdbc_practice_questions/question2/JdbcConnection.java
@@ -9,4 +9,19 @@ public class JdbcConnection {
     private static final String USERNAME="root";
     private static final String PASSWORD="";
     private static Connection connection=null;
+
+    public static Connection getConnection() {
+        return connection;
+    }
+
+    public boolean attemptConnection(){
+        boolean isConnected= false;
+        try {
+            connection=DriverManager.getConnection(URL,USERNAME,PASSWORD);
+            isConnected= true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return isConnected;
+    }
 }
