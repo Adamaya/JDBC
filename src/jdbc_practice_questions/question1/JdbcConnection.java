579c3df884604200b254d14f3e3c8b614cd842a5
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-12-05 18:52:19 +0530

    FIELDS Initialized

--- src/jdbc_practice_questions/question1/JdbcConnection.java
+++ src/jdbc_practice_questions/question1/JdbcConnection.java
@@ -5,4 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JdbcConnection {
+    private static final String URL="jdbc:mysql://localhost:3306/studentdatabase";
+    private static final String USERNAME="root";
+    private static final String PASSWORD="";
+    private static Connection connection=null;
 }
+
