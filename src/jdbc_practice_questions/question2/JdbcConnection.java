623b54daddf81cf794c2ad401c9c5fb91ae0725c
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-12-05 20:46:06 +0530

    fields created

--- src/jdbc_practice_questions/question2/JdbcConnection.java
+++ src/jdbc_practice_questions/question2/JdbcConnection.java
@@ -5,4 +5,8 @@ import java.sql.DriverManager;
 import java.sql.Connection;
 
 public class JdbcConnection {
+    private static final String URL="jdbc:mysql://localhost:3306/jdbc";
+    private static final String USERNAME="root";
+    private static final String PASSWORD="";
+    private static Connection connection=null;
 }
