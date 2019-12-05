60e32245b9ddb04392cce98945926e6fd7890273
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-12-05 20:42:36 +0530

    connection path changed

--- src/jdbc_practice_questions/question1/JdbcConnection.java
+++ src/jdbc_practice_questions/question1/JdbcConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JdbcConnection {
-    private static final String URL="jdbc:mysql://localhost:3306/studentdatabase";
+    private static final String URL="jdbc:mysql://localhost:3306/jdbc";
     private static final String USERNAME="root";
     private static final String PASSWORD="";
     private static Connection connection=null;
