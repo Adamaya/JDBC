44a746d26e42fd1b39be72a221535ef2df41fd14
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-12-06 15:09:11 +0530

    updateColumn, fetchData helper method created

--- src/jdbc_practice_questions/question7/Helper.java
+++ src/jdbc_practice_questions/question7/Helper.java
@@ -1,21 +1,42 @@
 package jdbc_practice_questions.question7;
 
-import java.sql.SQLException;
-import java.sql.PreparedStatement;
-import java.sql.Connection;
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class Helper {
     public boolean courseExist(Connection connection,String cName){
-        boolean isExist=false;
+        boolean isExist;
         try{
-            PreparedStatement preparedStatement= connection.prepareStatement("SELECT * FROM courses WHERE courseName=?;");
+            PreparedStatement preparedStatement= connection.prepareStatement("SELECT ? FROM courses;");
             preparedStatement.setString(1,cName);
             preparedStatement.executeUpdate();
             isExist=true;
         } catch (SQLException e) {
-            e.printStackTrace();
+            isExist=false;
         }
         return isExist;
+    }
+    public boolean updateColumn(Connection connection,String columnName,String courseName) {
+        boolean isUpdated;
+        isUpdated=false;
+        try{
+            PreparedStatement preparedStatement= connection.prepareStatement("SELECT ? FROM courses WHERE courseName=?;");
+            preparedStatement.setString(1,columnName);
+            preparedStatement.setString(2,courseName);
+            preparedStatement.executeUpdate();
+            isUpdated=true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return isUpdated;
+    }
+    public ResultSet fetchData(Connection connection){
+        ResultSet resultSet=null;
+        try{
+            Statement statement=connection.createStatement();
+            resultSet =statement.executeQuery("SELECT * FROM courses;");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultSet;
     }
 }
