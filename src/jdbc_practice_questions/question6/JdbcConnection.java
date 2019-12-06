7b584d6eb1615e8d7d11a63e31c7dd8834cf3980
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-12-06 13:02:59 +0530

    JdbcConnection class created

---
+++ src/jdbc_practice_questions/question6/JdbcConnection.java
@@ -0,0 +1,27 @@
+package jdbc_practice_questions.question6;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcConnection {
+    private static final String URL="jdbc:mysql://localhost:3306/jdbc";
+    private static final String USERNAME="root";
+    private static final String PASSWORD="";
+    private static Connection connection=null;
+
+    public static Connection getConnection() {
+        return connection;
+    }
+
+    public boolean attemptConnection(){
+        boolean isConnected= false;
+        try {
+            connection=DriverManager.getConnection(URL,USERNAME,PASSWORD);
+            isConnected= true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return isConnected;
+    }
+}
