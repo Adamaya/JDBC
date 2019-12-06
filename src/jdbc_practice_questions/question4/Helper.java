7af81936c262f74aeaa464c47e4033930d8b3aaf
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-12-06 09:43:55 +0530

    Helper class created

--- src/jdbc_practice_questions/question4/Helper.java
+++ src/jdbc_practice_questions/question4/Helper.java
@@ -1,4 +1,20 @@
 package jdbc_practice_questions.question4;
 
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Helper {
+    public ResultSet fetchSelectedData(Connection connection,String cName){
+        ResultSet resultSet=null;
+        try{
+            PreparedStatement preparedStatement=connection.prepareStatement("SELECT * FROM courses WHERE courseName=?;");
+            preparedStatement.setString(1,cName);
+            resultSet=preparedStatement.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
 }
