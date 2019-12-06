3206698216115a509d4796c26a202ee89cb2506f
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2019-12-06 09:26:10 +0530

    readAllCourseDetailsFromDatabase Helper methods created

--- src/jdbc_practice_questions/question3/Helper.java
+++ src/jdbc_practice_questions/question3/Helper.java
@@ -1,4 +1,37 @@
 package jdbc_practice_questions.question3;
 
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Helper {
+    public void readAllCourseDetailsFromDatabase(Connection connection){
+        ResultSet resultSet=null;
+        try{
+            Statement statement = connection.createStatement();
+            resultSet=statement.executeQuery("SELECT * FROM courses;");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        while (true){
+            try{
+                if(!resultSet.next()){
+                    break;
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try{
+                System.out.println("courseId: "+resultSet.getString("courseId")+" courseName: "
+                        +resultSet.getString("courseName")+" courseCode: " +resultSet.getString("courseCode")+" courseDuration: "
+                        +resultSet.getString("courseDuration")+" courseCost: "
+                        +resultSet.getString("courseCost"));
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
